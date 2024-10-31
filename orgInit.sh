a9e174c804ed54c8b3a8271212d99e6c92c70c02
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-31 16:40:03 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,6 @@
-#sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o prediction.demo
-sfdx force:org:create -f config/project-scratch-def.json -s -d 3 -w 60
-sfdx force:source:push
-sfdx force:user:permset:assign -n PredictionBuilder
-sfdx shane:data:tree:import -p data/plan.json -d data/
-sfdx force:user:password:generate
-sfdx force:org:open
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p prediction -e builder.demo
+sf project deploy start
+sf org assign permset -n PredictionBuilder
+sf data import tree -p data/plan.json
+sf demoutil user password set -p salesforce1 -g User -l User
+sf org open
